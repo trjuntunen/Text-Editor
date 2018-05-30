dc9d71e6b5804ef5ffcf582506f4cdcac3729219
trjuntunen <trjuntunen@gmail.com>
2018-05-30 16:12:56 -0700

    Added a menu bar and functionality to save the current text in the text area to a file, making this text editor technically complete. But much more functionality to be added.

--- TextEditor/src/Frame.java
+++ TextEditor/src/Frame.java
@@ -1,7 +1,14 @@
 import java.awt.Dimension;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
@@ -16,12 +23,18 @@ public class Frame {
 	private JTextArea textArea;
 	private JScrollPane scrollPane;
 	private TextLineNumber lineNumber;
+	private JMenuBar menuBar;
+	private JMenu menu;
+	private JMenuItem saveItem;
 
 	public Frame() {
 		frame = new JFrame();
 		textArea = new JTextArea();
 		scrollPane = new JScrollPane(textArea);
 		lineNumber = new TextLineNumber(textArea, 0);
+		menuBar = new JMenuBar();
+		menu = new JMenu("File");
+		saveItem = new JMenuItem("Size");
 	}
 
 	/**
@@ -30,6 +43,7 @@ public class Frame {
 	public void setup() {
 		setupTextArea();
 		setupScrollPane();
+		setupMenuBar();
 		setupFrame();
 	}
 
@@ -65,6 +79,27 @@ public class Frame {
 		textArea.setBorder(BorderFactory.createCompoundBorder(textArea.getBorder(), BorderFactory.createEmptyBorder(0, 5, 0, 0)));
 		textArea.setLineWrap(true);
 		textArea.setWrapStyleWord(true);
+	}
+
+	/**
+	 * Sets up the menu bar for drop down menu options.
+	 */
+	private void setupMenuBar() {
+		saveItem = menu.add("Save file to Desktop");
+
+		saveItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				String currentText = textArea.getText();
+				try(FileWriter writer = new FileWriter(new File("C:\\Users\\epbba\\Desktop\\Poppers.java"))) {
+					writer.write(currentText);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		menuBar.add(menu);
+		frame.setJMenuBar(menuBar);
 	}
 
 }
